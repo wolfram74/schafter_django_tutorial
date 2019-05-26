278c5675182331b72a22698876de8b45229d8fcf
wolfram74 <phaugen@gmail.com>
2019-05-26 10:00:12 -0500

    finished part 5

--- django_project/blog/admin.py
+++ django_project/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post
+# note for demo admin pass is test321
 
-# Register your models here.
+admin.site.register(Post)
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,31 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 import time
 import datetime
 
-today = datetime.date.today()
-yesterday = today + datetime.timedelta(days=-1)
-posts = [
-    {
-        'author':'perp1',
-        'title':'bost plog 1',
-        'content':'fumble buckets',
-        'date_posted':today,
-    },
-    {
-        'author':'perp2',
-        'title':'bost plog 4',
-        'content':'fumble truckets',
-        'date_posted':yesterday,
-    },
-]
-
+posts = Post.objects.all()
 
 def home(request):
     context = {
         'posts':posts
     }
-    print(str(posts[0]['date_posted']))
+    print(str(posts.first().date_posted))
     return render(request, 'blog/home.html', context)
 
 def about(request):
